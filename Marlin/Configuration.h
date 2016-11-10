897bf10f5fda68ca8bc8d6b97bb0989c82460d2d
Leif Bladt <lbladt@testo.de>
2016-11-10 23:45:11 +0100

    Adjust print area

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -608,9 +608,9 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 200
-#define Y_MAX_POS 200
-#define Z_MAX_POS 200
+#define X_MAX_POS 210
+#define Y_MAX_POS 210
+#define Z_MAX_POS 210
 
 //===========================================================================
 //========================= Filament Runout Sensor ==========================
